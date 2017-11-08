91e46f892a8abfa4024c70916f6dbe76690417b0
jacobshenning <jacobshenning@gmail.com>
2017-11-08 15:42:04 -0700

    Initial Commit

---
+++ VectorSet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VectorSet
+//
+//  Created by Alfred on 11/8/17.
+//  Copyright © 2017 C++ Class. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
