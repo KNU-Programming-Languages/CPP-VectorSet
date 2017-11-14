2d021d5bf8c6b750e4e94d9dbf3bc2062cc04d52
Jacob Henning <jacobshenning@gmail.com>
2017-11-14 00:16:28 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,107 +1,123 @@
 /**
- *
- *  @brief      Assignment tasked to create a class which acts a set.
- *
  *  @file       main.cpp
- *  @name       SetClass
+ *  @author     Jacob Henning
  *  @date       Created on 11/1/17
  *
- *  @author     Alfred
- *  @copyright  Copyright © 2017 Jacob Henning. All rights reserved.
- *  @license    MIT License
- *
- *  @notice     All of the following, unless otherwise noted in the attributes,
- *              is 100% orignal content created by Jacob Henning.
- *
- *  @attributes While this code has been 100% typed by Jacob Henning, some of
- *              the concepts and ideas may have been influenced by professors
- *              and classmates.
- *
+ *  @notice     All of the following, unless otherwise noted in file,
+ *              100% orignal content created by Jacob Henning
  */
 
-/// @include iostream
-#include <iostream>
-
-/// @include vector
 #include <vector>
-
-/// @include VectorSet
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
 #include "VectorSet.h"
 
-/// @namespace std
 using namespace std;
 
-//// test
+/**
+ *  Function designed to remove all non-alpha values from string
+ *
+ *  @param str is the string to be modified
+ *  @return result is the modified string
+ */
 
 string cleanUp(string str) {
     string result = "";
-    
+
     for (int i = 0; i < str.length(); i++)
         result += tolower(str[i]);
-    
+
     while (result.size()!=0 && !isalpha(result[0]))
         result = result.substr(1);
-    
+
     while (!isalpha(result.back()))
         result = result.substr(0, str.length() - 1);
-    
+
     return result;
 }
 
+/**
+ *  Function designed to console out all anagram combinations
+ *
+ *  @param word is the string to be analyzed
+ */
+
+void getAnagrams(string word) {
+    MySet<string> * arrangements = new VectorSet<string>;
+    string original = word;
+    sort(word.begin(), word.end());
+    arrangements->insert(word);
+    while (next_permutation(word.begin(), word.end()))
+        arrangements->insert(word);
+    cout << "There are " << arrangements->size() << " unique arra";
+    cout << "ngements of the word \"" << original << "\"" << endl;
+}
 
 /**
- *  @brief Does nothing specific, and is more or less of a playground.
+ *  Function designed to print out set in its entirety
+ *
+ *  @param arr is the array from the set, which will then be printed
+ */
+
+void printSet(int * arr) {
+    for (unsigned int i = 0; i < sizeof(*arr); i++)
+        cout << arr[i];
+    cout << endl;
+}
+
+/**
+ *  Main function can be used in whatever way needed, which in this case,
+ *  is to complete the requirements of this assignment.
  *
  *  @return 0
  */
 
 int main() {
     
+    ifstream fin("test.in");
+    
+    if (!fin) {
+        cerr << "File not found \n";
+        exit(1);
+    }
+    
+    string str;
+    
+    while (fin >> str) {
+        str = cleanUp(str);
+        getAnagrams(str);
+    }
+
     MySet<int> * intSet1 = new VectorSet<int>;
     MySet<int> * intSet2 = new VectorSet<int>;
-    
+
     intSet1->insert(1);
     intSet1->insert(2);
     intSet1->insert(3);
-    intSet1->insert(4);
-    intSet1->insert(5);
-    intSet1->insert(6);
-    intSet1->insert(7);
-    
-    // intSet1 = [1,2,3,4,5,6,7]        // 7
-    
-    intSet2->insert(6);
-    intSet2->insert(7);
-    intSet2->insert(8);
-    intSet2->insert(9);
-    intSet2->insert(10);
-    
-    // intSet2 = [6,7,8,9,10]               // 5
-    
+
+    intSet2->insert(3);
+    intSet2->insert(4);
+    intSet2->insert(5);
+
     MySet<int> * intSet3 = intSet1->set_union(*intSet2);
-    
-    // intSet3 = [1,2,3,4,5,6,7,8,9,10]     // 10
-    
     MySet<int> * intSet4 = intSet1->set_intersect(*intSet2);
-    
-    // intSet4 = [6,7]                      // 2
-    
     MySet<int> * intSet5 = intSet1->set_difference(*intSet2);
-    
-    // intSet5 = [1,2,3,4,5]                // 5
-    
     MySet<int> * intSet6 = intSet1->set_xor(*intSet2);
-    
-    // intSet6 = [1,2,3,4,5,8,9,10]         // 8
-    
-    cout << intSet1->size() << endl;
-    cout << intSet2->size() << endl;
-    cout << intSet3->size() << endl;
-    cout << intSet4->size() << endl;
-    cout << intSet5->size() << endl;
-    cout << intSet6->size() << endl;
-    cout << endl << endl << endl << endl;
-    
-    
+
+    cout << "Set 1: ";
+    printSet(intSet1->toArray());
+    cout << "Set 2: ";
+    printSet(intSet2->toArray());
+    cout << "Set 3: ";
+    printSet(intSet3->toArray());
+    cout << "Set 4: ";
+    printSet(intSet4->toArray());
+    cout << "Set 5: ";
+    printSet(intSet5->toArray());
+    cout << "Set 6: ";
+    printSet(intSet6->toArray());
+
     return 0;
 }
